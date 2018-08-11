ea5aa5b2c0d5861a6acc05a9bac9e14e1b0e5c90
egee-irl <brian@egee.io>
2018-08-11 12:06:20 -0700

    added grass for the background

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,15 +1,19 @@
 package;
 
+import flixel.util.FlxColor;
 import flixel.FlxState;
+import flixel.FlxG;
 
 class PlayState extends FlxState
 {
 	override public function create():Void
 	{
-		super.create();
-		var text = new flixel.text.FlxText(0, 0, "Hello World!", 64);
-		text.screenCenter();
-		add(text);
+		super.create(); // Not sure if this is needed..
+
+		FlxG.mouse.visible = false;
+		var background = new flixel.FlxSprite(0, 0);
+		background.loadGraphic("assets/images/GrassTextureTerrain1.jpg");
+		add(background);
 	}
 
 	override public function update(elapsed:Float):Void
